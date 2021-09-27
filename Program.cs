0a506ad3dc2245fd9a3fc3b21e5cbe027bd88f9c
OnurYucel2000 <39805114+OnurYucel05012000@users.noreply.github.com>
2021-09-27 11:02:08 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +13,20 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Game w = new Game();
-            w.Start();
-            Console.WriteLine("Oyun Bitti !");
+            int i = 0;
+            int gamelength = 20;
+            while (i<gamelength)
+            {                                              
+                    w.Start();
+                    Console.WriteLine("Oyun Bitti !");
+                i++;
+               
+            }
+            Console.WriteLine("=========");
+            Console.WriteLine("Owin :" + w.getOwin());
+            Console.WriteLine("Xwin :" + w.getXwin());
+            Console.WriteLine("Tie  :" + (gamelength - (w.getOwin() + w.getXwin()) )   );
+            Console.WriteLine("=========");
             Console.ReadKey();
             
         }
